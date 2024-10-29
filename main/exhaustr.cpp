c564a1972822f560920b8f852bdf1ffdf795096f
agxpro <herrscherofdarkness@gmail.com>
2024-10-29 20:23:21 +0530

    [main] Read DHT and Gas sensor values from another thread.
    Signed-off-by: agxpro <herrscherofdarkness@gmail.com>

--- main/exhaustr.cpp
+++ main/exhaustr.cpp
@@ -25,16 +25,27 @@ void* blink_thread(void* arg) {
   return nullptr;
 }
 
+void* dht_thread(void* args) {
+  DHTSensor dhtSensor1(GPIO_NUM_9, DHTSensor::SensorType::DHT_TYPE_DHT11, "DHT Sensor 1");
+  DHTSensor dhtSensor2(GPIO_NUM_10, DHTSensor::SensorType::DHT_TYPE_DHT11, "DHT Sensor 2");
+  GasSensor gasSensor(ADC1_CHANNEL_6 /* GPIO34 */, ADC_WIDTH_BIT_12);
+
+  while (true) {
+    float airQuality = gasSensor.read();
+    printf("Air Quality: %.2f\n", airQuality);
+
+    printf("DHT Sensor 1: Temperature: %.2f°C, Humidity: %.2f%%\n", dhtSensor1.getTemperature(), dhtSensor1.getHumidity());
+    printf("DHT Sensor 2: Temperature: %.2f°C, Humidity: %.2f%%\n", dhtSensor2.getTemperature(), dhtSensor2.getHumidity());
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
 void app_main() {
   CPUFan cpuFan(GPIO_NUM_15, "CPU Fan");
 
-  DHTSensor dhtSensor1(GPIO_NUM_9, DHTSensor::SensorType::DHT_TYPE_DHT11, "DHT Sensor 1");
-  DHTSensor dhtSensor2(GPIO_NUM_10, DHTSensor::SensorType::DHT_TYPE_DHT11, "DHT Sensor 2");
-
   CurrentSensor currentSensor(ADC1_CHANNEL_4 /* GPIO32 */, ADC_WIDTH_BIT_12);
   VoltageSensor voltageSensor(ADC1_CHANNEL_5 /* GPIO33 */, ADC_WIDTH_BIT_12);
-
-  GasSensor gasSensor(ADC1_CHANNEL_6 /* GPIO34 */, ADC_WIDTH_BIT_12);
 
   // Create a thread to blink the built-in LED
   esp_pthread_cfg_t cfg = esp_pthread_get_default_config();
@@ -45,14 +56,17 @@ void app_main() {
     printf("Failed to create thread: %d\n", res);
   }
 
+  // Create a thread to read data from the DHT sensors and gas sensor
+  res = pthread_create(&thread, nullptr, dht_thread, nullptr);
+  if (res != 0) {
+    printf("Failed to create thread: %d\n", res);
+  }
+
   while (true) {
     float current = currentSensor.read();
     printf("Current: %.2f A\n", current);
     float voltage = voltageSensor.read();
     printf("Voltage: %.2f V\n", voltage);
-
-    float airQuality = gasSensor.read();
-    printf("Air Quality: %.2f\n", airQuality);
 
     vTaskDelay(5000 / portTICK_PERIOD_MS);
     cpuFan.turnOn();
@@ -61,15 +75,6 @@ void app_main() {
     printf("Current: %.2f A\n", current);
     voltage = voltageSensor.read();
     printf("Voltage: %.2f V\n", voltage);
-
-    airQuality = gasSensor.read();
-    printf("Air Quality: %.2f\n", airQuality);
-
-    printf("Reading data from DHT sensor 1\n");
-    printf("Temperature: %.2f°C, Humidity: %.2f%%\n", dhtSensor1.getTemperature(), dhtSensor1.getHumidity());
-
-    printf("Reading data from DHT sensor 2\n");
-    printf("Temperature: %.2f°C, Humidity: %.2f%%\n", dhtSensor2.getTemperature(), dhtSensor2.getHumidity());
 
     vTaskDelay(15000 / portTICK_PERIOD_MS);
     cpuFan.turnOff();
