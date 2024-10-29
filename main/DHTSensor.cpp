10bb9cbb352fd92a8d26fbada9b7c97e1c595018
agxpro <herrscherofdarkness@gmail.com>
2024-10-29 13:18:04 +0530

    DHTSensor: Wait for ~2 sec before fetching new data from DHT11.
    Signed-off-by: agxpro <herrscherofdarkness@gmail.com>

--- main/DHTSensor.cpp
+++ main/DHTSensor.cpp
@@ -4,6 +4,7 @@
 #include "dht.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "esp_timer.h"
 
 #include "DHTSensor.hpp"
 
@@ -11,13 +12,30 @@ DHTSensor::DHTSensor(gpio_num_t gpioNum, dht_sensor_type_t sensorType, std::stri
   this->gpioNum = gpioNum;
   this->sensorType = sensorType;
   this->name = name;
+  this->lastReadTime = 0;
+  this->temperature = -1;
+  this->humidity = -1;
   gpio_reset_pin(gpioNum);
   gpio_set_direction(gpioNum, GPIO_MODE_INPUT);
 }
 
-void DHTSensor::read(float* temperature, float* humidity) {
-  esp_err_t err = dht_read_float_data(this->sensorType, this->gpioNum, humidity, temperature);
-  if (err != ESP_OK) {
+void DHTSensor::read() {
+  if (esp_timer_get_time() - this->lastReadTime < 2'000'000) {
+    ESP_LOGI("DHTSensor", "Skipping reading from %s sensor", this->name.c_str());
+    return;
+  }
+  esp_err_t err = dht_read_float_data(this->sensorType, this->gpioNum, &this->humidity, &this->temperature);
+  if (err == ESP_OK) {
+    this->lastReadTime = esp_timer_get_time();
+  } else {
+    this->temperature = -1;
+    this->humidity = -1;
     ESP_LOGE("DHTSensor", "Failed to read data from %s sensor", this->name.c_str());
   }
+}
+
+void DHTSensor::read(float* temperature, float* humidity) {
+  this->read();
+  *temperature = this->temperature;
+  *humidity = this->humidity;
 }
--- main/DHTSensor.hpp
+++ main/DHTSensor.hpp
@@ -4,12 +4,18 @@
 
 #include "driver/gpio.h"
 #include "dht.h"
+#include "esp_timer.h"
 
 class DHTSensor {
  private:
   gpio_num_t gpioNum;
   dht_sensor_type_t sensorType;
   std::string name;
+  int64_t lastReadTime;
+  float temperature;
+  float humidity;
+
+  void read();
 
  public:
   DHTSensor(gpio_num_t gpioNum, dht_sensor_type_t sensorType, std::string name);
