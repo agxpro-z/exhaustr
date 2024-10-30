30544ce2b96fb3440ef515aba97ef7fdd509d2d8
agxpro <herrscherofdarkness@gmail.com>
2024-10-30 22:58:53 +0530

    RESTServer: Add API to get and set fan state and speed.
    Signed-off-by: agxpro <herrscherofdarkness@gmail.com>

--- main/RESTServer.cpp
+++ main/RESTServer.cpp
@@ -13,14 +13,34 @@ static const char* TAG = "RESTServer";
 static const int REST_SERVER_PORT = 80;
 
 /*
-* Handler for the /chip/info URI
-*/
+ * Handler for the /chip/info URI
+ */
 static esp_err_t chipInfoHandler(httpd_req_t* req);
 
 /*
-* Device status handler /status URI
-*/
+ * Device status handler /status URI
+ */
 static esp_err_t statusHandler(httpd_req_t* req);
+
+/*
+ * Fan state GET handler /fan/state URI
+ */
+static esp_err_t fanStateGetHandler(httpd_req_t* req);
+
+/*
+ * Fan state POST handler /fan/state URI
+ */
+static esp_err_t fanStatePostHandler(httpd_req_t* req);
+
+/*
+ * Fan speed GET handler /fan/speed URI
+ */
+static esp_err_t fanSpeedGetHandler(httpd_req_t* req);
+
+/*
+ * Fan speed POST handler /fan/speed URI
+ */
+static esp_err_t fanSpeedPostHandler(httpd_req_t* req);
 
 void RESTServer::start() {
   // Start the REST server
@@ -54,6 +74,42 @@ void RESTServer::start() {
     .user_ctx = nullptr
   };
   httpd_register_uri_handler(server, &statusUri);
+
+  // Get the fan state
+  httpd_uri_t fanStateUri = {
+    .uri = "/fan/state",
+    .method = HTTP_GET,
+    .handler = fanStateGetHandler,
+    .user_ctx = nullptr
+  };
+  httpd_register_uri_handler(server, &fanStateUri);
+
+  // Set the fan state
+  httpd_uri_t fanStatePostUri = {
+    .uri = "/fan/state",
+    .method = HTTP_POST,
+    .handler = fanStatePostHandler,
+    .user_ctx = nullptr
+  };
+  httpd_register_uri_handler(server, &fanStatePostUri);
+
+  // Get the fan speed
+  httpd_uri_t fanSpeedUri = {
+    .uri = "/fan/speed",
+    .method = HTTP_GET,
+    .handler = fanSpeedGetHandler,
+    .user_ctx = nullptr
+  };
+  httpd_register_uri_handler(server, &fanSpeedUri);
+
+  // Set the fan speed
+  httpd_uri_t fanSpeedPostUri = {
+    .uri = "/fan/speed",
+    .method = HTTP_POST,
+    .handler = fanSpeedPostHandler,
+    .user_ctx = nullptr
+  };
+  httpd_register_uri_handler(server, &fanSpeedPostUri);
 }
 
 // Handler for the /chip/info URI
@@ -134,5 +190,132 @@ static esp_err_t statusHandler(httpd_req_t* req) {
   httpd_resp_set_type(req, "application/json");
   httpd_resp_send(req, json, strlen(json));
   free(json);
+  return ESP_OK;
+}
+
+// Fan state GET handler /fan/state URI
+static esp_err_t fanStateGetHandler(httpd_req_t* req) {
+  DeviceController* deviceController = DeviceController::getInstance();
+  cJSON* root = cJSON_CreateObject();
+  cJSON_AddStringToObject(root, "state", deviceController->getCPUFan().isOn() ? "on" : "off");
+
+  char* json = cJSON_Print(root);
+  cJSON_Delete(root);
+
+  httpd_resp_set_type(req, "application/json");
+  httpd_resp_send(req, json, strlen(json));
+  free(json);
+  return ESP_OK;
+}
+
+// Fan state POST handler /fan/state URI
+static esp_err_t fanStatePostHandler(httpd_req_t* req) {
+  DeviceController* deviceController = DeviceController::getInstance();
+  char buf[1024];
+  int ret = httpd_req_recv(req, buf, sizeof(buf));
+  if (ret <= 0) {
+    if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+      httpd_resp_send_408(req);
+    }
+    return ESP_FAIL;
+  }
+  buf[ret] = '\0';
+
+  cJSON* root = cJSON_Parse(buf);
+  if (root == nullptr) {
+    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Invalid JSON");
+    return ESP_FAIL;
+  }
+
+  cJSON* state = cJSON_GetObjectItem(root, "state");
+  if (state == nullptr or !cJSON_IsString(state)) {
+    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Invalid fan state! Must be a string");
+    cJSON_Delete(root);
+    return ESP_FAIL;
+  }
+
+  if (strcmp(state->valuestring, "on") == 0) {
+    if (deviceController->getCPUFan().isOn()) {
+      httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Fan is already on");
+      cJSON_Delete(root);
+      return ESP_FAIL;
+    }
+    deviceController->getCPUFan().turnOn();
+  } else if (strcmp(state->valuestring, "off") == 0) {
+    if (not deviceController->getCPUFan().isOn()) {
+      httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Fan is already off");
+      cJSON_Delete(root);
+      return ESP_FAIL;
+    }
+    deviceController->getCPUFan().turnOff();
+  } else {
+    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Invalid fan state! Must be 'on' or 'off'");
+    cJSON_Delete(root);
+    return ESP_FAIL;
+  }
+
+  cJSON_Delete(root);
+  httpd_resp_send(req, nullptr, 0);
+
+  return ESP_OK;
+}
+
+// Fan speed GET handler /fan/speed URI
+static esp_err_t fanSpeedGetHandler(httpd_req_t* req) {
+  DeviceController* deviceController = DeviceController::getInstance();
+  cJSON* root = cJSON_CreateObject();
+  cJSON_AddNumberToObject(root, "speed", deviceController->getCPUFan().getFanSpeed());
+
+  char* json = cJSON_Print(root);
+  cJSON_Delete(root);
+
+  httpd_resp_set_type(req, "application/json");
+  httpd_resp_send(req, json, strlen(json));
+  free(json);
+  return ESP_OK;
+}
+
+// Fan speed POST handler /fan/speed URI
+static esp_err_t fanSpeedPostHandler(httpd_req_t* req) {
+  DeviceController* deviceController = DeviceController::getInstance();
+  char buf[1024];
+  int ret = httpd_req_recv(req, buf, sizeof(buf));
+  if (ret <= 0) {
+    if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+      httpd_resp_send_408(req);
+    }
+    return ESP_FAIL;
+  }
+  buf[ret] = '\0';
+
+  cJSON* root = cJSON_Parse(buf);
+  if (root == nullptr) {
+    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Invalid JSON");
+    return ESP_FAIL;
+  }
+
+  cJSON* speed = cJSON_GetObjectItem(root, "speed");
+  if (speed == nullptr or !cJSON_IsNumber(speed)) {
+    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Invalid fan speed! Must be a number");
+    cJSON_Delete(root);
+    return ESP_FAIL;
+  }
+
+  if (speed->valueint < 1 or speed->valueint > 100) {
+    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Invalid fan speed! Must be between 1 and 100");
+    cJSON_Delete(root);
+    return ESP_FAIL;
+  }
+
+  if (not deviceController->getCPUFan().isOn()) {
+    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Fan is off! Turn it on first");
+    cJSON_Delete(root);
+    return ESP_FAIL;
+  }
+
+  deviceController->getCPUFan().setFanSpeed(speed->valueint);
+  cJSON_Delete(root);
+
+  httpd_resp_send(req, nullptr, 0);
   return ESP_OK;
 }
